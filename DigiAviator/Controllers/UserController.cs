0021e1b899410aeada857e0c3c3a620167c54980
alexandersavov <savov.alxndr@gmail.com>
2022-03-16 23:37:15 +0200

    Home page finished, added User controller with Register and Login views.

---
+++ DigiAviator/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using DigiAviator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DigiAviator.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public UserController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
