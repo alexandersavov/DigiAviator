adc5d01fd245a9589c6db2957f52b8c51f8b961f
alexandersavov <savov.alxndr@gmail.com>
2022-03-23 00:35:03 +0200

    FlightPreparation controller and AirspaceMap view added.

---
+++ DigiAviator/Controllers/FlightPreparationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigiAviator.Controllers
+{
+    public class FlightPreparationController : BaseController
+    {
+        public IActionResult AirspaceMap()
+        {
+            return View();
+        }
+    }
+}
